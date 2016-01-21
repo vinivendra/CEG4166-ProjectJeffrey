9364b140f517e8c3b60eedcbd0046a968173dd26
vinivendra <vinivendra@gmail.com>
2016-01-20 21:11:05 -0500

    Initial commit

---
+++ NewProject/main.c
@@ -0,0 +1,102 @@
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////    main.c
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <avr/io.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+/* serial interface include file. */
+#include "usartserial.h"
+
+
+/*-----------------------------------------------------------*/
+/* Create a handle for the serial port. */
+//extern xComPortHandle xSerialPort;
+
+static void TaskBlinkRedLED(void *pvParameters); // Main Arduino Mega 2560, Freetronics EtherMega (Red) LED Blink
+
+/*-----------------------------------------------------------*/
+
+/* Main program loop */
+//int main(void) __attribute__((OS_main));
+int usartfd;
+
+int main(void)
+{
+	// Comment
+
+    // turn on the serial port for debugging or for other USART reasons.
+	usartfd = usartOpen( USART0_ID, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+
+	usart_print_P(PSTR("\r\n\n\nHello World!\r\n")); // Ok, so we're alive...
+
+    xTaskCreate(
+		TaskBlinkRedLED
+		,  (const portCHAR *)"RedLED" // Main Arduino Mega 2560, Freetronics EtherMega (Red) LED Blink
+		,  256				// Tested 9 free @ 208
+		,  NULL
+		,  3
+		,  NULL ); // */
+
+	usart_printf_P(PSTR("\r\n\nFree Heap Size: %u\r\n"),xPortGetFreeHeapSize() ); // needs heap_1 or heap_2 for this function to succeed.
+
+	vTaskStartScheduler();
+
+	usart_print_P(PSTR("\r\n\n\nGoodbye... no space for idle task!\r\n")); // Doh, so we're dead...
+
+}
+
+/*-----------------------------------------------------------*/
+
+
+static void TaskBlinkRedLED(void *pvParameters) // Main Red LED Flash
+{
+    (void) pvParameters;;
+    TickType_t xLastWakeTime;
+
+	/* The xLastWakeTime variable needs to be initialised with the current tick
+	count.  Note that this is the only time we access this variable.  From this
+	point on xLastWakeTime is managed automatically by the vTaskDelayUntil()
+	API function. */
+	xLastWakeTime = xTaskGetTickCount();
+
+	DDRB |= _BV(DDB7);
+
+    while(1)
+    {
+    	PORTB |=  _BV(PORTB7);       // main (red IO_B7) LED on. EtherMega LED on
+		vTaskDelayUntil( &xLastWakeTime, ( 100 / portTICK_PERIOD_MS ) );
+
+		PORTB &= ~_BV(PORTB7);       // main (red IO_B7) LED off. EtherMega LED off
+		vTaskDelayUntil( &xLastWakeTime, ( 400 / portTICK_PERIOD_MS ) );
+
+		usart_printf_P(PSTR("RedLED HighWater @ %u\r\n"), uxTaskGetStackHighWaterMark(NULL));
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask,
+                                    portCHAR *pcTaskName )
+{
+
+	DDRB  |= _BV(DDB7);
+	PORTB |= _BV(PORTB7);       // main (red PB7) LED on. Mega main LED on and die.
+	while(1);
+}
+
+/*-----------------------------------------------------------*/
+
